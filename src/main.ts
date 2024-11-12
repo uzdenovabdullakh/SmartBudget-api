5486922aafc9a95dbc048a8804b931cafc2f820f
uzdenovabdullakh <uzdenovabdullah@yandex.ru>
2024-11-12 21:08:10 +0300

    chore: [KB-32] add config service, create env.example file

--- src/main.ts
+++ src/main.ts
@@ -1,13 +1,15 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import { ConfigService } from '@nestjs/config';
 
 async function bootstrap() {
-  const PORT = process.env.PORT || 5000;
-
   const app = await NestFactory.create(AppModule, {
     logger: console,
   });
   app.enableCors();
+
+  const configService = app.get(ConfigService);
+  const PORT = configService.get('PORT') || 5000;
 
   await app.listen(PORT, () => console.log(`Server started on port = ${PORT}`));
 }
