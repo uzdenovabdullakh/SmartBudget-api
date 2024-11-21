4dc901cd2e7d7d54341708f36b747775b5395368
uzdenovabdullakh <uzdenovabdullah@yandex.ru>
2024-11-21 13:10:16 +0300

    docs: [KB-25] install nestjs-swagger, setup swagger

--- src/main.ts
+++ src/main.ts
@@ -4,6 +4,7 @@ import { ConfigService } from '@nestjs/config';
 import { WinstonModule } from 'nest-winston';
 import { instance } from './logger/winston.logger';
 import { HttpExceptionFilter } from './exceptions/exception.filter';
+import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule, {
@@ -16,6 +17,21 @@ async function bootstrap() {
 
   const configService = app.get(ConfigService);
   const PORT = configService.get('PORT') || 5000;
+
+  const config = new DocumentBuilder()
+    .setTitle('SmartBudget Docs')
+    .setDescription('SmartBudget API documentation')
+    .setExternalDoc('Swagger in json', '/api-docs-json')
+    .setVersion('1.0')
+    .addBearerAuth()
+    .build();
+  const documentFactory = () => SwaggerModule.createDocument(app, config);
+  SwaggerModule.setup('/api-docs', app, documentFactory, {
+    swaggerOptions: {
+      persistAuthorization: true,
+    },
+    explorer: true,
+  });
 
   await app.listen(PORT, () => console.log(`Server started on port = ${PORT}`));
 }
