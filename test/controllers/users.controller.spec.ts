86e5c78c94174b384a5f82c5d4ab7852de3d4ccd
uzdenovabdullakh <uzdenovabdullah@yandex.ru>
2024-11-23 18:05:51 +0300

    test: [KB-40] add mocking

--- test/controllers/users.controller.spec.ts
+++ test/controllers/users.controller.spec.ts
@@ -1,21 +1,41 @@
 import { Test, TestingModule } from '@nestjs/testing';
 import { UsersController } from 'src/controllers/users.controller';
 import { UsersService } from 'src/services/users.service';
-import { describe, beforeEach, it, expect } from 'vitest';
+import { User } from 'src/entities/user.entity';
+import { getRepositoryToken } from '@nestjs/typeorm';
+import { describe, beforeEach, it, expect, vi } from 'vitest';
 
 describe('UsersController', () => {
   let controller: UsersController;
+  let service: UsersService;
+
+  const mockUserRepository = {
+    findOne: vi.fn(),
+    create: vi.fn(),
+    save: vi.fn(),
+    update: vi.fn(),
+    softDelete: vi.fn(),
+    restore: vi.fn(),
+  };
 
   beforeEach(async () => {
     const module: TestingModule = await Test.createTestingModule({
       controllers: [UsersController],
-      providers: [UsersService],
+      providers: [
+        UsersService,
+        {
+          provide: getRepositoryToken(User),
+          useValue: mockUserRepository,
+        },
+      ],
     }).compile();
 
     controller = module.get<UsersController>(UsersController);
+    service = module.get<UsersService>(UsersService);
   });
 
   it('should be defined', () => {
     expect(controller).toBeDefined();
+    expect(service).toBeDefined();
   });
 });
--- test/services/users.service.spec.ts
+++ test/services/users.service.spec.ts
@@ -1,13 +1,30 @@
 import { Test, TestingModule } from '@nestjs/testing';
 import { UsersService } from 'src/services/users.service';
-import { describe, beforeEach, it, expect } from 'vitest';
+import { getRepositoryToken } from '@nestjs/typeorm';
+import { User } from 'src/entities/user.entity';
+import { describe, beforeEach, it, expect, vi } from 'vitest';
 
 describe('UsersService', () => {
   let service: UsersService;
 
+  const mockUserRepository = {
+    findOne: vi.fn(),
+    create: vi.fn(),
+    save: vi.fn(),
+    update: vi.fn(),
+    softDelete: vi.fn(),
+    restore: vi.fn(),
+  };
+
   beforeEach(async () => {
     const module: TestingModule = await Test.createTestingModule({
-      providers: [UsersService],
+      providers: [
+        UsersService,
+        {
+          provide: getRepositoryToken(User),
+          useValue: mockUserRepository,
+        },
+      ],
     }).compile();
 
     service = module.get<UsersService>(UsersService);
@@ -15,5 +32,21 @@ describe('UsersService', () => {
 
   it('should be defined', () => {
     expect(service).toBeDefined();
+  });
+
+  it('should call findOne method in repository', async () => {
+    const userId = '123';
+    mockUserRepository.findOne.mockResolvedValue({
+      id: userId,
+      email: 'test@example.com',
+    });
+
+    const result = await service.findOne(userId);
+
+    expect(mockUserRepository.findOne).toHaveBeenCalledWith({
+      where: { id: userId },
+      select: ['id', 'login', 'email', 'settings', 'isActivated'],
+    });
+    expect(result).toEqual({ id: userId, email: 'test@example.com' });
   });
 });
