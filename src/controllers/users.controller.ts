fe148b6217f5f36bfdaeb79b2114fb0e18fb0496
uzdenovabdullakh <uzdenovabdullah@yandex.ru>
2024-11-23 17:18:21 +0300

    feat: [KB-40] add validation schemas, validation pipe in users controller

--- src/controllers/users.controller.ts
+++ src/controllers/users.controller.ts
@@ -9,18 +9,24 @@ import {
   Res,
   ParseUUIDPipe,
   HttpStatus,
+  UsePipes,
 } from '@nestjs/common';
 import { Response } from 'express';
+import { ZodValidationPipe } from 'src/pipes/validation-pipe';
 import { UsersService } from 'src/services/users.service';
 import { CreateUserDto } from 'src/types/dto/create-user.dto';
 import { RestoreUserDto } from 'src/types/dto/restore-user.dto';
 import { UpdateUserDto } from 'src/types/dto/update-user.dto';
+import { CreateUserSchema } from 'src/validation/create-user.schema';
+import { RestoreUserSchema } from 'src/validation/restore-user.schema';
+import { UpdateUserSchema } from 'src/validation/update-user.schema';
 
 @Controller('users')
 export class UsersController {
   constructor(private readonly usersService: UsersService) {}
 
   @Post()
+  @UsePipes(new ZodValidationPipe(CreateUserSchema))
   async create(@Res() res: Response, @Body() dto: CreateUserDto) {
     const data = await this.usersService.create(dto);
     return res.status(HttpStatus.CREATED).send({
@@ -39,7 +45,7 @@ export class UsersController {
   async update(
     @Res() res: Response,
     @Param('id', ParseUUIDPipe) id: string,
-    @Body() dto: UpdateUserDto,
+    @Body(new ZodValidationPipe(UpdateUserSchema)) dto: UpdateUserDto,
   ) {
     const data = await this.usersService.update(id, dto);
     return res.status(HttpStatus.OK).send({
@@ -57,6 +63,7 @@ export class UsersController {
   }
 
   @Post('restore')
+  @UsePipes(new ZodValidationPipe(RestoreUserSchema))
   async restore(@Res() res: Response, @Body() dto: RestoreUserDto) {
     await this.usersService.restore(dto);
     return res.status(HttpStatus.OK).send({
