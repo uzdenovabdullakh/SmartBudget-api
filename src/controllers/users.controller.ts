c0ef5612c7f5be5102749cef7ef7e039aaf30ef5
uzdenovabdullakh <uzdenovabdullah@yandex.ru>
2024-11-24 11:32:59 +0300

    feat: [KB-21] remove user create endpoint, add findOneByEmail service

--- src/controllers/users.controller.ts
+++ src/controllers/users.controller.ts
@@ -14,26 +14,14 @@ import {
 import { Response } from 'express';
 import { ZodValidationPipe } from 'src/pipes/validation-pipe';
 import { UsersService } from 'src/services/users.service';
-import { CreateUserDto } from 'src/types/dto/create-user.dto';
 import { RestoreUserDto } from 'src/types/dto/restore-user.dto';
 import { UpdateUserDto } from 'src/types/dto/update-user.dto';
-import { CreateUserSchema } from 'src/validation/create-user.schema';
 import { RestoreUserSchema } from 'src/validation/restore-user.schema';
 import { UpdateUserSchema } from 'src/validation/update-user.schema';
 
 @Controller('users')
 export class UsersController {
   constructor(private readonly usersService: UsersService) {}
-
-  @Post()
-  @UsePipes(new ZodValidationPipe(CreateUserSchema))
-  async create(@Res() res: Response, @Body() dto: CreateUserDto) {
-    const data = await this.usersService.create(dto);
-    return res.status(HttpStatus.CREATED).send({
-      data,
-      message: 'User was successfully created',
-    });
-  }
 
   @Get(':id')
   async findOne(@Res() res: Response, @Param('id', ParseUUIDPipe) id: string) {
