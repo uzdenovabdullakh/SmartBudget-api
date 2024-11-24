46ee01a43d2acf9d5c12246ec62ad989c3506b99
uzdenovabdullakh <uzdenovabdullah@yandex.ru>
2024-11-24 17:42:50 +0300

    refactor: [KB-21] refactor user controllers

--- src/controllers/users.controller.ts
+++ src/controllers/users.controller.ts
@@ -4,16 +4,17 @@ import {
   Post,
   Body,
   Patch,
-  Param,
   Delete,
   Res,
-  ParseUUIDPipe,
   HttpStatus,
   UsePipes,
+  Req,
 } from '@nestjs/common';
 import { Response } from 'express';
+import { Public } from 'src/decorators/public.decorator';
 import { ZodValidationPipe } from 'src/pipes/validation-pipe';
 import { UsersService } from 'src/services/users.service';
+import { AuthenticationRequest } from 'src/types/authentication-request.types';
 import { RestoreUserDto } from 'src/types/dto/restore-user.dto';
 import { UpdateUserDto } from 'src/types/dto/update-user.dto';
 import { RestoreUserSchema } from 'src/validation/restore-user.schema';
@@ -23,33 +24,34 @@ import { UpdateUserSchema } from 'src/validation/update-user.schema';
 export class UsersController {
   constructor(private readonly usersService: UsersService) {}
 
-  @Get(':id')
-  async findOne(@Res() res: Response, @Param('id', ParseUUIDPipe) id: string) {
-    const user = await this.usersService.findOne(id);
+  @Get()
+  async findOne(@Req() req: AuthenticationRequest, @Res() res: Response) {
+    const user = await this.usersService.findOne(req.user.id);
     return res.status(HttpStatus.OK).send(user);
   }
 
-  @Patch(':id')
+  @Patch()
   async update(
+    @Req() req: AuthenticationRequest,
     @Res() res: Response,
-    @Param('id', ParseUUIDPipe) id: string,
     @Body(new ZodValidationPipe(UpdateUserSchema)) dto: UpdateUserDto,
   ) {
-    const data = await this.usersService.update(id, dto);
+    const data = await this.usersService.update(req.user.id, dto);
     return res.status(HttpStatus.OK).send({
       data,
       message: 'User was successfully updated',
     });
   }
 
-  @Delete(':id')
-  async remove(@Res() res: Response, @Param('id', ParseUUIDPipe) id: string) {
-    await this.usersService.remove(id);
+  @Delete()
+  async remove(@Req() req: AuthenticationRequest, @Res() res: Response) {
+    await this.usersService.remove(req.user.id);
     return res.status(HttpStatus.OK).send({
       message: 'User was successfully removed',
     });
   }
 
+  @Public()
   @Post('restore')
   @UsePipes(new ZodValidationPipe(RestoreUserSchema))
   async restore(@Res() res: Response, @Body() dto: RestoreUserDto) {
