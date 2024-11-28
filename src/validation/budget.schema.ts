ed0084396fdcd30ee97ab22a229fbad38e737887
uzdenovabdullakh <uzdenovabdullah@yandex.ru>
2024-11-28 21:38:08 +0300

    refactor: create ArrayOfIdsSchema

--- src/validation/budget.schema.ts
+++ src/validation/budget.schema.ts
@@ -1,4 +1,5 @@
 import { z } from 'zod';
+import { ArrayOfIdsSchema } from './array-of-ids.schema';
 
 const BudgetSettingsSchema = z.object({
   currency: z.enum(['USD', 'RUB', 'EUR']),
@@ -11,11 +12,6 @@ export const CreateBudgetSchema = z.object({
 });
 
 export const UpdateBudgetSchema = CreateBudgetSchema.partial().optional();
-
-export const BudgetsIdsSchema = z
-  .array(z.string().uuid({ message: 'Invalid UUID format' }))
-  .min(1, { message: 'Array must contain at least one UUID' })
-  .max(10, { message: 'Array must not contain more than 10 UUIDs' });
 
 export type CreateBudgetDto = z.infer<typeof CreateBudgetSchema>;
 export type UpdateBudgetDto = z.infer<typeof UpdateBudgetSchema>;
