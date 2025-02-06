3e43617e5b7b110539b5943df379830d65c1bf4d
uzdenovabdullakh <uzdenovabdullah@yandex.ru>
2025-02-06 16:36:51 +0300

    style: fix max length in CreateUnlinkedAccountSchema

--- src/validation/account.schema.ts
+++ src/validation/account.schema.ts
@@ -5,7 +5,7 @@ import { z } from 'src/utils/zod-map';
 export const CreateUnlinkedAccountSchema = z.object({
   name: z
     .string()
-    .max(1, i18next.t('validation.Name too long', { ns: 'common' }))
+    .max(128, i18next.t('validation.Name too long', { ns: 'common' }))
     .min(1, i18next.t('validation.Name is required', { ns: 'common' })),
   budgetId: z
     .string()
