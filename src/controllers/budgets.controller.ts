1b1aa2eaaa003328c9f2ff472c3968ff43c364a8
uzdenovabdullakh <uzdenovabdullah@yandex.ru>
2025-02-09 21:29:08 +0300

    refactor: [KB-57] remove response from mutation budget, add selecr in get requests

--- src/controllers/budgets.controller.ts
+++ src/controllers/budgets.controller.ts
@@ -35,9 +35,8 @@ export class BudgetsController {
     @Body() dto: CreateBudgetDto,
     @Req() req: AuthenticationRequest,
   ) {
-    const data = await this.budgetService.createBudget(dto, req.user);
+    await this.budgetService.createBudget(dto, req.user);
     return {
-      data,
       message: this.t.tMessage('created', 'budget'),
     };
   }
@@ -66,9 +65,8 @@ export class BudgetsController {
     @Body(new ZodValidationPipe(UpdateBudgetSchema)) dto: UpdateBudgetDto,
     @Req() req: AuthenticationRequest,
   ) {
-    const data = await this.budgetService.updateBudget(id, dto, req.user);
+    await this.budgetService.updateBudget(id, dto, req.user);
     return {
-      data,
       message: this.t.tMessage('updated', 'budget'),
     };
   }
