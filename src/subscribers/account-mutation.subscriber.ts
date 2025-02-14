239fdb1236df37e1bb9b0c21aa15c4cb1a409cf8
uzdenovabdullakh <uzdenovabdullah@yandex.ru>
2025-02-14 20:48:08 +0300

    feat: [KB-115] change default category on account mutation

---
+++ src/subscribers/account-mutation.subscriber.ts
@@ -0,0 +1,54 @@
+import {
+  EntityManager,
+  EntitySubscriberInterface,
+  EventSubscriber,
+  InsertEvent,
+  RemoveEvent,
+} from 'typeorm';
+import { Account } from 'src/entities/account.entity';
+import { Category } from 'src/entities/category.entity';
+
+@EventSubscriber()
+export class AccountSubscriber implements EntitySubscriberInterface<Account> {
+  listenTo() {
+    return Account;
+  }
+
+  async afterInsert(event: InsertEvent<Account>) {
+    const { entity, manager } = event;
+    await this.updateAccountAndCategory(entity, manager);
+  }
+
+  async afterSoftRemove(event: RemoveEvent<Account>) {
+    const { entity, manager } = event;
+    await this.updateAccountAndCategory(entity, manager, true);
+  }
+
+  private async updateAccountAndCategory(
+    account: Account,
+    manager: EntityManager,
+    isRemoved: boolean = false,
+  ) {
+    await manager.connection.transaction(async (manager) => {
+      const categoryRepository = manager.getRepository(Category);
+
+      const category = await categoryRepository.findOne({
+        where: {
+          name: 'Inflow: Ready to Assign',
+          budget: {
+            id: account.budget.id,
+          },
+        },
+      });
+
+      const amount = isRemoved ? -account.amount : account.amount;
+
+      await categoryRepository.update(
+        { id: category.id },
+        {
+          assigned: category.assigned + amount,
+        },
+      );
+    });
+  }
+}
