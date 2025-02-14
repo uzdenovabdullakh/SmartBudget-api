695b73db9f2f468c0695703adbdf88c41e036a73
uzdenovabdullakh <uzdenovabdullah@yandex.ru>
2025-02-14 20:05:32 +0300

    feat: [KB-102] remove assignedAmount from budget, add default category and category group creating

--- src/entities/budget.entity.ts
+++ src/entities/budget.entity.ts
@@ -14,7 +14,6 @@ import { Goal } from './goal.entity';
 import { Analytic } from './analytic.entity';
 import { BudgetSettings } from 'src/validation/budget.schema';
 import { Category } from './category.entity';
-import { NumericTransformer } from 'src/utils/numeric-transformer';
 
 @Entity({ name: 'budgets' })
 export class Budget extends Timestamps {
@@ -30,16 +29,6 @@ export class Budget extends Timestamps {
     type: 'jsonb',
   })
   settings: BudgetSettings;
-
-  @Column({
-    default: 0,
-    type: 'numeric',
-    precision: 10,
-    scale: 2,
-    name: 'assigned_amount',
-    transformer: new NumericTransformer(),
-  })
-  assignedAmount: number;
 
   @ManyToOne(() => User, { onDelete: 'CASCADE', nullable: false })
   @JoinColumn({
