130f97f78e50376810f2707e5a88b13cffb08447
uzdenovabdullakh <uzdenovabdullah@yandex.ru>
2025-03-01 14:20:36 +0300

    feat: create bulkFindOrCreate service, add category in import transaction

--- src/modules/transactions.module.ts
+++ src/modules/transactions.module.ts
@@ -1,14 +1,15 @@
 import { Module } from '@nestjs/common';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { TransactionsController } from 'src/controllers/transactions.controller';
-import { Account } from 'src/entities/account.entity';
 import { Budget } from 'src/entities/budget.entity';
 import { Transaction } from 'src/entities/transaction.entity';
 import { TransactionsService } from 'src/services/transactions.service';
+import { CategoriesModule } from './categories.module';
 
 @Module({
-  imports: [TypeOrmModule.forFeature([Transaction, Budget, Account])],
+  imports: [TypeOrmModule.forFeature([Transaction, Budget]), CategoriesModule],
   controllers: [TransactionsController],
   providers: [TransactionsService],
+  exports: [TransactionsService],
 })
 export class TransactionsModule {}
